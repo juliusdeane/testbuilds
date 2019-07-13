7c5d808feff33a9eee26dfb3158b27f19b2bb33c
juliusdeane <48096490+juliusdeane@users.noreply.github.com>
2019-07-13 10:14:12 +0200

    Initial

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc -o uno uno.c
+
+clean:
+	rm -rf uno
---
+++ uno.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *env[]) {
+    printf("This is a test line.\n");
+    return 0;
+}
+
